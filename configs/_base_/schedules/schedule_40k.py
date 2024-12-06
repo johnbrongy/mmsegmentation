fb95b188a523e714a191ff29fe72f952fb245e1b
johnbrongy <168834802+johnbrongy@users.noreply.github.com>
2024-12-06 22:17:17 +0800

    Update schedule_40k.py

--- configs/_base_/schedules/schedule_40k.py
+++ configs/_base_/schedules/schedule_40k.py
@@ -12,7 +12,7 @@ param_scheduler = [
         by_epoch=False)
 ]
 # training schedule for 40k
-train_cfg = dict(type='IterBasedTrainLoop', max_iters=40000, val_interval=4000)
+train_cfg = dict(type='IterBasedTrainLoop', max_iters=1500, val_interval=300)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 default_hooks = dict(
