6b619f6cee53d6c1ccb7f1de4bb85053416ce93e
johnbrongy <168834802+johnbrongy@users.noreply.github.com>
2024-12-09 23:28:22 +0800

    Update schedule_40k.py

--- configs/_base_/schedules/schedule_40k.py
+++ configs/_base_/schedules/schedule_40k.py
@@ -1,5 +1,5 @@
 # optimizer
-optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0005)
+optimizer = dict(type='SGD', lr=0.0005, momentum=0.9, weight_decay=0.0005)
 optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
 # learning policy
 param_scheduler = [
