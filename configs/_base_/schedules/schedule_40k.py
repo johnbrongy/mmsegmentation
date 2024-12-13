bd96aaa5ac7ee3e3e5c87825b0a09479c3e40dd9
johnbrongy <168834802+johnbrongy@users.noreply.github.com>
2024-12-13 19:20:46 +0800

    Update schedule_40k.py

--- configs/_base_/schedules/schedule_40k.py
+++ configs/_base_/schedules/schedule_40k.py
@@ -4,19 +4,20 @@ optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
 # learning policy
 param_scheduler = [
     dict(
-        type='CosineAnnealingLR',
+        type='PolyLR',
         eta_min=1e-4,
+        power=0.9,
         begin=0,
         end=40000,
         by_epoch=False)
 ]
 # training schedule for 40k
-train_cfg = dict(type='IterBasedTrainLoop', max_iters=1500, val_interval=750)
+train_cfg = dict(type='IterBasedTrainLoop', max_iters=40000, val_interval=4000)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 default_hooks = dict(
     timer=dict(type='IterTimerHook'),
-    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
+    logger=dict(type='LoggerHook', interval=40, log_metric_by_epoch=False),
     param_scheduler=dict(type='ParamSchedulerHook'),
     checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=4000),
     sampler_seed=dict(type='DistSamplerSeedHook'),
