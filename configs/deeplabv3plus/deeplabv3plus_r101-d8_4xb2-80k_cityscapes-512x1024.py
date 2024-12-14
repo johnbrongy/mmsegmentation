2c24b82ce33a17c801145bd283212f7b295db8c5
johnbrongy <168834802+johnbrongy@users.noreply.github.com>
2024-12-14 11:59:37 +0800

    Update deeplabv3plus_r101-d8_4xb2-80k_cityscapes-512x1024.py

--- configs/deeplabv3plus/deeplabv3plus_r101-d8_4xb2-80k_cityscapes-512x1024.py
+++ configs/deeplabv3plus/deeplabv3plus_r101-d8_4xb2-80k_cityscapes-512x1024.py
@@ -1,2 +1,5 @@
 _base_ = './deeplabv3plus_r50-d8_4xb2-80k_cityscapes-512x1024.py'
-model = dict(pretrained='open-mmlab://resnet101_v1c', backbone=dict(depth=101))
+model = dict(pretrained='open-mmlab://resnet101_v1c', backbone=dict(depth=101, cbam_ratio = 8, cbam_kernel_size = 7))
+
+checkpoint_config = dict(interval=4000, max_keep_ckpts=10)
+work_dir = '/content/drive/MyDrive/deeplabv3+_cbam_natocc'
