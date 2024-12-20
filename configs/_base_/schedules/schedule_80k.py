bd5a72941dfd858a842ba2ca4627f9cd73cee004
johnbrongy <168834802+johnbrongy@users.noreply.github.com>
2024-12-20 20:38:49 +0800

    Update schedule_80k.py

--- configs/_base_/schedules/schedule_80k.py
+++ configs/_base_/schedules/schedule_80k.py
@@ -1,16 +1,24 @@
 # optimizer
-optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0008)
-optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
+optimizer = dict(type='AdamW', lr=0.005, momentum=0.9, weight_decay=0.001)
+optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=dict(max_norm=35, norm_type=2))
 # learning policy
+
 param_scheduler = [
+    dict(
+        type='LinearLR',  # Warm-up for the first 1000 iterations
+        start_factor=0.001,
+        begin=0,
+        end=1000,
+        by_epoch=False),
     dict(
         type='PolyLR',
         eta_min=1e-4,
         power=0.9,
-        begin=0,
+        begin=1000,
         end=80000,
         by_epoch=False)
 ]
+
 # training schedule for 80k
 train_cfg = dict(type='IterBasedTrainLoop', max_iters=80000, val_interval=4000)
 val_cfg = dict(type='ValLoop')
