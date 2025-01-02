817754b46bc17d0cc4d60e66480b0ac26e7e7360
johnbrongy <168834802+johnbrongy@users.noreply.github.com>
2025-01-02 13:38:14 +0800

    Update cityscapes.py

--- configs/_base_/datasets/cityscapes.py
+++ configs/_base_/datasets/cityscapes.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'CityscapesDataset'
-data_root = 'data/cityscapes/cityscapes_natOcc_without_SOT'
+data_root = 'data/cityscapes/cityscapes_natOcc_withoutSOT'
 crop_size = (512, 1024)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
